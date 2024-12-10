bfcefded88ed77012ae9e31650e28ce8c503ceef
META DREAMER <jutt@ualberta.ca>
2024-12-10 14:29:26 -0700

    Fix import for active proposals

--- src/components/active-proposals.tsx
+++ src/components/active-proposals.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { ChevronRight, Users, Coins, MessageSquare } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
 
 interface Proposal {
